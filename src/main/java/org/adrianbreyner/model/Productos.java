752f9ede2eb9f78785795b8e18063ba478b9feef
eGalv3zDev <acamposeco-2024018@kinal.edu.gt>
2025-07-08 20:07:41 -0600

    Modelo
    agregue el modelo para hacer uso de la conexion

--- src/main/java/org/adrianbreyner/model/Productos.java
+++ src/main/java/org/adrianbreyner/model/Productos.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.adrianbreyner.model;
 
-/**
- *
- * @author PC
- */
 public class Productos {
-    
+    private int idProducto;
+    private String producto;
+    private String descripcion;
+    private int stock;
+    private double precio;
+ 
+    public Productos() {
+    }
+ 
+    public Productos(int idProducto, String producto, String descripcion, int stock, double precio) {
+        this.idProducto = idProducto;
+        this.producto = producto;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.precio = precio;
+    }
+ 
+    public int getIdProducto() {
+        return idProducto;
+    }
+ 
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+ 
+    public String getProducto() {
+        return producto;
+    }
+ 
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+ 
+    public String getDescripcion() {
+        return descripcion;
+    }
+ 
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+ 
+    public int getStock() {
+        return stock;
+    }
+ 
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+ 
+    public double getPrecio() {
+        return precio;
+    }
+ 
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 }
